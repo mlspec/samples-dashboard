1c94da4f5877b5a7bab6d2eca7509227cd5a6430
David Aronchick <aronchick@gmail.com>
2020-05-27 18:10:29 +0000

    Updates

---
+++ precache-manifest.884d49ab8271eeec941d00e782638f4a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf0fe257c221047a5f639dfa7e4eb831",
+    "url": "/samples-dashboard/index.html"
+  },
+  {
+    "revision": "2cc8d8e687e9220fa5ff",
+    "url": "/samples-dashboard/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "bce4554d7167f6720f32",
+    "url": "/samples-dashboard/static/js/2.d1ce64f4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/samples-dashboard/static/js/2.d1ce64f4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2cc8d8e687e9220fa5ff",
+    "url": "/samples-dashboard/static/js/main.1cc713e2.chunk.js"
+  },
+  {
+    "revision": "8bfe535e43db13397f7a",
+    "url": "/samples-dashboard/static/js/runtime-main.3991035f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/samples-dashboard/static/media/logo.5d5d9eef.svg"
+  }
+]);
