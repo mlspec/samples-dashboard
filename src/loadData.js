5d97b2188415c182a42f1e7598fb8ec6996e0534
David Aronchick <aronchick@gmail.com>
2020-06-01 18:13:39 +0000

    adding sample ids

--- src/loadData.js
+++ src/loadData.js
@@ -10,7 +10,7 @@ endpoint = "ws://gha-and-aml-test.gremlin.cosmos.azure.com:8182/"
 primaryKey = "UGgIOr2sEoli6sh0JrMKe1nw4Csuf5sY61ujoEwnuz50Q3D5FrroGnpQfsgo2s37Bs3g2lbHgcNwXtKU7mELZg=="
 database = "metadata_store"
 collection = "workflows_testing"
-`) 
+`)
 // readFile(filepath: string, encoding?: string)
 // RNFS.readFile('./config.toml', 'utf-8').then(res => {
 //     config_full = toml.parse(res);
@@ -40,15 +40,15 @@ const newPerson = () => {
       statusChance > 0.66
         ? 'relationship'
         : statusChance > 0.33
-        ? 'complicated'
-        : 'single',
+          ? 'complicated'
+          : 'single',
   }
 }
 
-const newWorkflow = () => 
-{
-  
-
+const newWorkflow = (id_value) => {
+  return {
+    id: id_value
+  };
 }
 
 export default function makeData(...lens) {
@@ -70,62 +70,62 @@ export default function makeData(...lens) {
 
 export class WorkflowData {
 
-    constructor() {
-        var authenticator = new Gremlin.driver.auth.PlainTextSaslAuthenticator(`/dbs/${config.database}/colls/${config.collection}`, config.primaryKey);
-        this.gremlin_config_options = {
-            authenticator,
-            traversalsource: "g",
-            rejectUnauthorized: true,
-            mimeType: "application/vnd.gremlin-v2.0+json"
-        }
-
-        // this.client = new Gremlin.driver.Client(
-        //     config.endpoint,
-        //     this.gremlin_config_options
-        // );
-        // const gremlin = require('gremlin');
-        var DriverRemoteConnection = Gremlin.driver.DriverRemoteConnection;
-        this.Graph = Gremlin.structure.Graph;
-        
-        this.gremlin_websocket = new WebSocket('ws://gha-and-aml-test.gremlin.cosmos.azure.com:8182/')
-
-        // var dc = new DriverRemoteConnection(this.gremlin_websocket,this.gremlin_config_options);
-
-        this.graph = new this.Graph();
-        this.g = this.graph.traversal().withRemote(this.gremlin_websocket);
-      };
-
-
-    executeAsyncQuery = function (client, q) {
-        return new Promise(function (resolve) {
-            console.log(`Running ${q}`);
-            client.open = () => resolve(client.send(q, {}));
-        });
-    };
-
-    get_all_objects = function () {
-        // this.executeAsyncQuery("g.V()");
-        return this.executeAsyncQuery(this.gremlin_websocket, "g.V()");
-    };
-
-    foo = async function () {
-        this.client.open();
-        var result = await this.executeAsyncQuery(this.client, "g.V()");
-        console.log('Woo done!', result);
-        this.client.close();
-        // But the best part is, we can just keep awaiting different stuff, without ugly .then()s
-        // var somethingElse = await getSomethingElse()
-        // var moreThings = await getMoreThings()
+  constructor() {
+    var authenticator = new Gremlin.driver.auth.PlainTextSaslAuthenticator(`/dbs/${config.database}/colls/${config.collection}`, config.primaryKey);
+    this.gremlin_config_options = {
+      authenticator,
+      traversalsource: "g",
+      rejectUnauthorized: true,
+      mimeType: "application/vnd.gremlin-v2.0+json"
     }
 
-    // (async function () {
-    //     client.open();
-    //     var result = await executeAsyncQuery(q);
-    //     console.log('Woo done!', result);
+    // this.client = new Gremlin.driver.Client(
+    //     config.endpoint,
+    //     this.gremlin_config_options
+    // );
+    // const gremlin = require('gremlin');
+    var DriverRemoteConnection = Gremlin.driver.DriverRemoteConnection;
+    this.Graph = Gremlin.structure.Graph;
 
-    //     // But the best part is, we can just keep awaiting different stuff, without ugly .then()s
-    //     // var somethingElse = await getSomethingElse()
-    //     // var moreThings = await getMoreThings()
-    // })()
+    this.gremlin_websocket = new WebSocket('wss://gha-and-aml-test.gremlin.cosmos.azure.com:8182/gremlin')
+
+    // var dc = new DriverRemoteConnection(this.gremlin_websocket,this.gremlin_config_options);
+
+    this.graph = new this.Graph();
+    this.g = this.graph.traversal().withRemote(this.gremlin_websocket);
+  };
+
+
+  executeAsyncQuery = function (client, q) {
+    return new Promise(function (resolve) {
+      console.log(`Running ${q}`);
+      client.open = () => resolve(client.send(q, {}));
+    });
+  };
+
+  get_all_objects = function () {
+    // this.executeAsyncQuery("g.V()");
+    return this.executeAsyncQuery(this.gremlin_websocket, "g.V()");
+  };
+
+  foo = async function () {
+    this.client.open();
+    var result = await this.executeAsyncQuery(this.client, "g.V()");
+    console.log('Woo done!', result);
+    this.client.close();
+    // But the best part is, we can just keep awaiting different stuff, without ugly .then()s
+    // var somethingElse = await getSomethingElse()
+    // var moreThings = await getMoreThings()
+  }
+
+  // (async function () {
+  //     client.open();
+  //     var result = await executeAsyncQuery(q);
+  //     console.log('Woo done!', result);
+
+  //     // But the best part is, we can just keep awaiting different stuff, without ugly .then()s
+  //     // var somethingElse = await getSomethingElse()
+  //     // var moreThings = await getMoreThings()
+  // })()
 
 }
